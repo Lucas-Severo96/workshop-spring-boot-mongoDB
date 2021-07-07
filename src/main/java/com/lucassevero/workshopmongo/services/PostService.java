ec43cf03f9a320499c2f2bb8173875b24574b392
Lucas Severo <lucasleite_14.1@hotmail.com>
2021-07-06 22:40:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lucassevero/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lucassevero.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucassevero.workshopmongo.domain.Post;
+import com.lucassevero.workshopmongo.repository.PostRepository;
+import com.lucassevero.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById (String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
